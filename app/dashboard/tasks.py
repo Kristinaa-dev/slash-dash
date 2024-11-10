fef604d72a055a302e9e521f6a5c989b1f71833e
Kristina Subjakova <44373311+Kristinaa-dev@users.noreply.github.com>
2024-11-10 15:17:51 -0500

    Add celery collection task

---
+++ app/dashboard/tasks.py
@@ -0,0 +1,7 @@
+# dashboard/tasks.py
+from celery import shared_task
+from django.core.management import call_command
+
+@shared_task
+def run_collect_metrics():
+    call_command("collect_metrics")
