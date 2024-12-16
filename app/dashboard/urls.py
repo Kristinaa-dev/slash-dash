1eefe5ed2cd2a192e5b393eec96447e6e9a45999
Kristina Subjakova <44373311+Kristinaa-dev@users.noreply.github.com>
2024-12-16 22:42:31 +0100

    Add endpoint for docker interaction

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -5,7 +5,8 @@ from .views import docker_monitor
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # path('system-data/', views.system_data, name='system_data'),
-    path('docker-monitor/', docker_monitor, name='docker_monitor'),
+    path('docker-monitor/', views.docker_monitor, name='docker_monitor'),
+    path('docker-action/<str:action>/<str:container_id>/', views.container_action, name='container_action'),
     path("terminal/", views.terminal_view, name="terminal"),
     # path('test', views.dashboard, name='dashboard'),
     path('latest-data/', views.latest_data, name='latest_data'),
