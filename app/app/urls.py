aefa328c96d8f1162202139b1b944bad37632c63
Kristina Subjakova <44373311+Kristinaa-dev@users.noreply.github.com>
2025-01-04 03:10:49 +0100

    Link django admin to settings

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     
     
-    path('admin/', admin.site.urls),
+    path('settings/', admin.site.urls),
     path('', include('dashboard.urls')),
 
 ]
