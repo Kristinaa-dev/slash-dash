a16dc6058fcca76b93520184bf7bc18bd62d0807
Kristina Subjakova <44373311+Kristinaa-dev@users.noreply.github.com>
2025-01-16 23:08:45 +0100

    Add alert creation form

--- app/dashboard/forms.py
+++ app/dashboard/forms.py
@@ -5,3 +5,10 @@ class NodeForm(forms.ModelForm):
     class Meta:
         model = Node
         fields = ['name', 'ip_address', 'node_type', 'location', 'ssh_username', 'ssh_password']
+
+from .models import AlertRule
+
+class AlertRuleForm(forms.ModelForm):
+    class Meta:
+        model = AlertRule
+        fields = ['node', 'metric_type', 'comparison_type', 'threshold', 'is_active']
