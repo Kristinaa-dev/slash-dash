a75e0d1258016521a0177be9fc6ccd0c757599f2
Kristina Subjakova <44373311+Kristinaa-dev@users.noreply.github.com>
2025-01-29 01:46:22 +0100

    Checker for alerts

--- app/dashboard/signals.py
+++ app/dashboard/signals.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def compare(value, threshold, comparison_type):
+    print("ALERT")
     if comparison_type == '>':
         return value > threshold
     elif comparison_type == '<':
@@ -28,9 +29,7 @@ def check_alerts_on_new_timeseriesdata(sender, instance, created, **kwargs):
     """Checks and triggers alerts when a new TimeSeriesData is created."""
     if not created:
         return
-
-    # print(f"Received post_save signal for TimeSeriesData ID: {instance.id}")
-
+    
     # Fetch relevant AlertRules
     relevant_rules = AlertRule.objects.filter(
         node=instance.node,
@@ -38,12 +37,6 @@ def check_alerts_on_new_timeseriesdata(sender, instance, created, **kwargs):
         is_active=True
     ).select_related('node', 'metric_type', 'user')
 
-    # print(f"Found {relevant_rules.count()} relevant AlertRules for TimeSeriesData ID: {instance.id}")
-
     for rule in relevant_rules:
         if compare(instance.value, rule.threshold, rule.comparison_type):
-            # print(f"AlertRule ID {rule.id} triggered by TimeSeriesData ID {instance.id}")
             rule.trigger_alert(current_value=instance.value)
-        else:
-            # print(f"AlertRule ID {rule.id} not triggered by TimeSeriesData ID {instance.id}")
-            pass
